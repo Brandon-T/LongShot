a17b06748ddaf4d9164c67761deb552fb3ea1d7c
Soner Yuksel <soner@plasticmobile.com>
2019-06-19 14:16:15 -0400

    Merge Sort Algorithm Implementation

--- LongShot/Structures & Algorithms/Algorithms.swift
+++ LongShot/Structures & Algorithms/Algorithms.swift
@@ -59,3 +59,97 @@ func insertionSort<T: Comparable>(_ array: inout [T]) {
     }
     
 }
+
+
+/// Implementation of Merge Sort Algorithm using a generic Array
+///
+/// Merge sort is a s an efficient, general-purpose, comparison-based sorting algorithm
+///
+/// Merge sort is a divide and conquer algorithm
+///
+/// In sorting n objects, merge sort has an average and worst-case performance of O(n log n)
+///
+/// Merge sort is more efficient than quicksort for some types of lists
+/// if the data to be sorted can only be efficiently accessed sequentially
+///
+/// How it works: Conceptually, a merge sort works as follows:
+/// Divide the unsorted list into n sublists, each containing one element
+/// Repeatedly merge sublists to produce new sorted sublists until there is only one sublist remaining.
+///
+
+/// Usage:
+///
+/// var testArrayMergeSort = [8, 2, 5, 3, 11, 4, 6]
+///
+/// let sortedArray = mergeSort(testArrayMergeSort)
+/// print("Sorted \(sortedArray)") -> 2, 3, 4, 5, 6, 8, 11
+///
+///      ->  Divide array into pieces and small chunks until it is single element
+///
+///             8 - 2 - 5 - 3    11 - 4 - 6 - 1
+///             8 - 2   5 - 3
+///                 8 - 2
+///
+///      ->  Merge them back by sorting
+
+
+func mergeSort<T: Comparable>(_ array: [T]) -> [T] {
+    
+    /// Exit condition where array has at least 2 elements
+    guard array.count > 1 else {
+        return array
+    }
+    
+    /// Find the middle point
+    let middleIndex = array.count / 2
+    
+    /// Create left- right array using array slices
+    /// Divide an Conquer part of Merge Sort
+    let leftPile = mergeSort(Array(array.prefix(upTo: middleIndex)))
+    let rightPile =  mergeSort(Array(array.suffix(from: middleIndex)))
+    
+    return merge(leftPile, rightPile)
+}
+
+
+// Merge Function that needs to be sorting and merging both sides
+func merge<T: Comparable>(_ leftArray: [T], _ rightArray: [T]) -> [T] {
+    
+    /// The Indexes used for either side of the slices
+    var leftIndex = 0
+    var rightIndex = 0
+    
+    var resultArray = [T]()
+    
+    /// Traverse from left to right in slices and increment side index
+    /// According to which one is smaller and added to the resultArray
+    while leftIndex < leftArray.count && rightIndex < rightArray.count {
+        if leftArray[leftIndex] < rightArray[rightIndex] {
+            
+            resultArray.append(leftArray[leftIndex])
+            leftIndex += 1
+        }
+        else if (leftArray[leftIndex] > rightArray[rightIndex]) {
+            resultArray.append(rightArray[rightIndex])
+            rightIndex += 1
+        }
+        else {
+            resultArray.append(leftArray[leftIndex])
+            resultArray.append(rightArray[rightIndex])
+            
+            leftIndex += 1
+            rightIndex += 1
+        }
+        
+    }
+    
+    /// Add the rest of the content in slices if one side is already added to resultArray
+    if leftIndex == leftArray.count {
+        resultArray.append(contentsOf: rightArray[rightIndex...])
+    } else {
+        resultArray.append(contentsOf: leftArray[leftIndex...])
+    }
+    
+    
+    return resultArray
+}
